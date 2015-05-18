8aff489d889b52fe3e161ab6e713253bb7f7406e
Christian Sieber <kazanir@gmail.com>
2015-05-18 11:24:27 +0000

    Initial commit, provisioning script outline, vhost files, and PHP7 conf

---
+++ scripts/provision.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password mysql'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password mysql'
+
+echo "deb http://archive.ubuntu.com/ubuntu trusty multiverse
+deb http://archive.ubuntu.com/ubuntu trusty-updates multiverse
+deb http://security.ubuntu.com/ubuntu trusty-security multiverse" | sudo tee -a /etc/apt/sources.list > /dev/null
+
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+sudo add-apt-repository -y 'deb http://dl.hhvm.com/ubuntu trusty main'
+
+sudo apt-get update -y
+
+sudo apt-get install -y accountsservice adduser apache2-mpm-worker \
+  apache2-utils apparmor apt apt-transport-https apt-utils autoconf automake \
+  bash bash-completion build-essential bzip2 ca-certificates cmake coreutils \
+  default-jre dos2unix dpkg e2fsprogs ed eject findutils gcc-4.8 \
+  geoip-database git git-flow gitk glances gnupg gpgsm gpgv grep hhvm iperf \
+  libapache2-mod-fastcgi libmcrypt-dev libmemcached-dev libmysqlclient-dev \
+  libtool makedev man-db manpages mawk memcached mime-support mlocate \
+  module-init-tools mount mountall mtr multiarch-support mysql-common \
+  mysql-server ncdu ncurses-base ncurses-bin ocaml ocaml-native-compilers \
+  openntpd openssh-client openssh-server openssl php5 php5-cli php5-curl \
+  php5-dbg php5-fpm php5-gd php5-ldap php5-mcrypt php5-mysql php5-memcache \
+  php5-pgsql php5-redis php5-xdebug putty-tools redis-server resolvconf \
+  rsync rsyslog samba screen sed siege solr-tomcat tcpdump tmux traceroute \
+  udev ufw unixodbc-dev vagrant vim vim-common vim-tiny wget xml-core zsh \
+  libcurl4-openssl-dev libmcrypt-dev libxml2-dev libjpeg-dev libfreetype6-dev \
+  libmysqlclient-dev libt1-dev libgmp-dev libpspell-dev libicu-dev \
+  librecode-dev
+
+# Permissions chicanery
+sudo usermod -a -G www-data $USER
+sudo chown -R www-data:www-data /var/log/apache2
+sudo chown www-data:www-data /var/log/php5-fpm.log
+
+# MySQL user setup
+mysql -u root -pmysql -e "CREATE USER '$USER'@'localhost';"
+mysql -u root -pmysql -e "GRANT ALL ON *.* TO '$USER'@'localhost';"
+mysql -u root -pmysql -e "FLUSH PRIVILEGES;"
+
+
+# PHP5 setup
+sudo php5enmod mcrypt pdo_mysql mysql mysqli
+sudo service php5-fpm restart
+# @todo: PHP5 config files
+
+# @todo: PHP7 setup
+wget http://repos.zend.com/zend-server/early-access/php7/php-7.0-latest-DEB-x86_64.tar.gz
+sudo tar zxPf php-7.*.tar.gz
+sudo cp ~/maat/php7/etc-initd-php7fpm /etc/init.d/php7-fpm
+sudo chmod a+x /etc/init.d/php7-fpm
+sudo cp ~/maat/php7/etc-init-php7fpm /etc/init/php7-fpm
+sudo cp ~/maat/php7/php7-fpm-checkconf /usr/local/lib/php7-fpm-checkconf
+sudo chmod a+x /usr/local/lib/php7-fpm-checkconf
+sudo update-rc.d php7-fpm defaults
+sudo service php7-fpm start
+
+# @todo: PHP7 config files
+
+# Apache config
+sudo a2dismod mpm_event
+sudo a2enmod alias actions vhost_alias rewrite mpm_worker ssl socache_shmcb proxy fastcgi
+# @todo: Wildcard VHosts for PHP5, PHP7, HHVM
+sudo cp ~/maat/apache2/vhosts/*.conf /etc/apache2/sites-available/
+sudo a2ensite php5.conf php7.conf hhvm.conf
+
+# @todo: Wildcard DNS for *.benchmark
+
+# Restart Apache
+sudo apache2ctl restart
+
+# Composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+composer global require drush/drush:dev-master
+echo 'export PATH="$PATH:$HOME/.composer/vendor/bin"' >> /home/$USER/.bashrc
+
+# Use my Vim config! It rules.
+# git clone https://github.com/Kazanir/dotvim.git ~/.vim
+# ln -s ~/.vim/.vimrc ~/.vimrc
+# cd ~/.vim
+# git submodule init
+# git submodule update
+
+
