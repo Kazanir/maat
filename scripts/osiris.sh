16005c20b8578ad4ef4d7b6100a7e90128640f21
Christian Sieber <kazanir@gmail.com>
2015-06-04 02:25:32 +0000

    Perf batch running script and supporting plumbing

---
+++ scripts/osiris.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Do setup.
+CURRENT_DATE=`date +"%Y-%m-%d"`
+mkdir -p ~/maat/results
+sudo mv /etc/php5/mods-available/opcache.ini ~/maat/tools/oss/opcache.ini
+sed -i 's/save_comments=0/save_comments=1/' ~/oss/conf/php.ini
+cp ~/oss/base/PerfSettings.php ~/maat/tools/oss/PerfSettingsBackup.php
+
+echo "***** Running OSS batch with concurrency 1..."
+
+cp ~/maat/tools/oss/PerfC1.php ~/oss/base/PerfSettings.php
+hhvm ~/oss/batch-run.php < ~/maat/tools/batch.json > ~/maat/results/results_c1_${CURRENT_DATE}.json
+
+echo "***** Running OSS batch with concurrency 5..."
+
+cp ~/maat/tools/oss/PerfC5.php ~/oss/base/PerfSettings.php
+hhvm ~/oss/batch-run.php < ~/maat/tools/batch.json > ~/maat/results/results_c5_${CURRENT_DATE}.json
+
+echo "***** Running OSS batch with concurrency 20..."
+
+cp ~/maat/tools/oss/PerfC20.php ~/oss/base/PerfSettings.php
+hhvm ~/oss/batch-run.php < ~/maat/tools/batch.json > ~/maat/results/results_c20_${CURRENT_DATE}.json
+
+echo "***** Cleaning up..."
+
+cp ~/maat/tools/oss/PerfSettingsBackup.php ~/oss/base/PerfSettings.php
+sudo mv ~/maat/tools/oss/opcache.ini /etc/php5/mods-available/opcache.ini
+
---
+++ tools/oss/PerfC5.php
@@ -0,0 +1,52 @@
+<?hh
+/*
+ *  Copyright (c) 2014, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+final class PerfSettings {
+
+  ///// Benchmark Settings /////
+
+  // Per concurrent thread - so, total number of requests made during warmup
+  // is WarmupRequests * WarmupConcurrency
+  public static function WarmupRequests(): int {
+    return 300;
+  }
+
+  public static function WarmupConcurrency(): int {
+    return 1;
+  }
+
+  public static function BenchmarkTime(): string {
+    // [0-9]+[SMH]
+    return '1M'; // 1 minute
+  }
+
+  public static function BenchmarkConcurrency(): int {
+    return 5;
+  }
+
+  ///// Server Settings /////
+
+  public static function HttpPort(): int {
+    return 8090;
+  }
+
+  public static function HttpAdminPort(): int {
+    return 8091;
+  }
+
+  public static function BackendPort(): int {
+    return 8092;
+  }
+
+  public static function BackendAdminPort(): int {
+    return 8093;
+  }
+}
