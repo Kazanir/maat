aa2fc1d1414d448e5121dd9e2892f73b9725edc0
Christian Sieber <kazanir@gmail.com>
2015-06-23 21:30:16 +0200

    Script upgrades

---
+++ scripts/postresults.php
@@ -0,0 +1,93 @@
+<?hh
+
+run();
+
+function run(): void {
+  foreach (scandir(results_dir()) as $file) {
+    if ($file != "." && $file != "..") {
+      process_results_file($file);
+    }
+  }
+}
+
+<<__Memoize>>
+function commit_hash(): string {
+  return getenv("D8_ACTUAL_COMMIT");
+}
+
+<<__Memoize>>
+function commit_time(): int {
+  return (int) getenv("D8_COMMIT_TIME");
+}
+
+<<__Memoize>>
+function results_dir(): string {
+  return __DIR__ . '/../results/';
+}
+
+<<__Memoize>>
+function runtime_labels(): Map<string, string> {
+  $labels = Map {
+    "php5" => "PHP 5.6.9",
+    "php7" => "PHP 7 (2015-05-24)",
+    "hhvm" => "HHVM 3.7.2 Repo.Auth",
+  };
+
+  return $labels;
+}
+
+function get_concurrency(string $filepath): string {
+  $stuff = explode('_', $filepath);
+  $c = substr($stuff[1], 1);
+
+  return $c;
+}
+
+function process_results_file(string $filepath): void {
+  $json = file_get_contents(results_dir() . $filepath);
+  $concurrency = get_concurrency($filepath);
+  $results = json_decode($json, TRUE);
+  if (!$results) {
+    return;
+  }
+  foreach ($results as $target => $runtimes) {
+    process_target($target, $concurrency, $runtimes);
+  }
+}
+
+function process_target(string $target, string $concurrency, array $runtimes): void {
+  $labels = runtime_labels();
+
+  foreach ($runtimes as $runtime => $results) {
+    $comb = $results['Combined'];
+    $r = Map {};
+    $r["rps"] = $comb["Siege RPS"];
+    $r["requests"] = $comb["Siege requests"];
+    $r["requests_success"] = $comb["Siege successful requests"];
+    $r["requests_failed"] = $comb["Siege failed requests"];
+    $r["nginx_hits"] = $comb["Nginx hits"];
+    $r["bytes"] = $comb["Nginx avg bytes"];
+    $r["time"] = (string) round($comb["Nginx avg time"] * 1000, 4);
+    $r["target"] = $target;
+    $r["runtime"] = $runtime;
+    $r["runtime_label"] = $labels[$runtime];
+    $r["commit"] = commit_hash();
+    $r["commit_time"] = commit_time();
+    $r["concurrency"] = $concurrency;
+
+    $http = Map {};
+    foreach ($comb as $key => $value) {
+      $code = substr($key, 6, 3);
+      if (substr($key, 0, 5) == "Nginx" && is_numeric($code)) {
+        $http[$code] = $value;
+      }
+    }
+    $r["responses"] = $http;
+
+    post_results($r);
+  }
+}
+
+function post_results(Map<string, mixed> $result): void {
+  print_r($result);
+}
--- scripts/thoth.sh
+++ scripts/thoth.sh
@@ -8,7 +8,8 @@ if [ "$D8_COMMIT" != "none" ]; then
   git clone https://github.com/drupal/drupal.git ~/drupal-8.0.0-beta11
   cd ~/drupal-8.0.0-beta11
   git checkout $D8_COMMIT
-  D8_ACTUAL_COMMIT=`git rev-parse HEAD`
+  D8_ACTUAL_COMMIT=`git rev-parse --short HEAD`
+  D8_COMMIT_TIME=$(git show -s --format=%ct $D8_ACTUAL_COMMIT)
   tar -C /home/ubuntu -czf ~/drupal-8.0.0-beta11.tar.gz drupal-8.0.0-beta11
   mv ~/drupal-8.0.0-beta11.tar.gz ~/oss/targets/drupal8/drupal-8.0.0-beta11.tar.gz
 fi
@@ -21,23 +22,25 @@ echo -e "\n********************************************"
 echo -e "***** Running D8 batch with concurrency 1..."
 
 cp ~/maat/tools/oss/PerfC1.php ~/oss/base/PerfSettings.php
-hhvm ~/oss/batch-run.php --i-am-not-benchmarking --no-proxygen < ~/maat/tools/d8_progress.json > ~/maat/results/results_c1_${CURRENT_DATE}.json
+hhvm ~/oss/batch-run.php --i-am-not-benchmarking --no-proxygen < ~/maat/tools/d8_progress.json > ~/maat/results/results_c1_${D8_ACTUAL_COMMIT}_${CURRENT_DATE}.json
 
 echo -e "\n********************************************"
 echo -e "***** Running D8 batch with concurrency 5..."
 
 cp ~/maat/tools/oss/PerfC5.php ~/oss/base/PerfSettings.php
-hhvm ~/oss/batch-run.php --i-am-not-benchmarking --no-proxygen < ~/maat/tools/d8_progress.json > ~/maat/results/results_c5_${CURRENT_DATE}.json
+hhvm ~/oss/batch-run.php --i-am-not-benchmarking --no-proxygen < ~/maat/tools/d8_progress.json > ~/maat/results/results_c5_${D8_ACTUAL_COMMIT}_${CURRENT_DATE}.json
 
 echo -e "\n*********************************************"
 echo -e "***** Running D8 batch with concurrency 20..."
 
 cp ~/maat/tools/oss/PerfC20.php ~/oss/base/PerfSettings.php
-hhvm ~/oss/batch-run.php --i-am-not-benchmarking --no-proxygen < ~/maat/tools/d8_progress.json > ~/maat/results/results_c20_${CURRENT_DATE}.json
+hhvm ~/oss/batch-run.php --i-am-not-benchmarking --no-proxygen < ~/maat/tools/d8_progress.json > ~/maat/results/results_c20_${D8_ACTUAL_COMMIT}_${CURRENT_DATE}.json
 
 echo -e "\n*************************************"
 echo -e "***** Posting data to API endpoint..."
 
+export D8_ACTUAL_COMMIT
+export D8_COMMIT_TIME
 hhvm ~/maat/scripts/postresults.php
 
 echo -e "\n***** Cleaning up..."
